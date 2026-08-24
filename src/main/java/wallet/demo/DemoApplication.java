c090859b8e25b2a22daa9522b87f0b1298197c43
Mohd Aasif <aasifmohd1385@gmail.com>
2026-08-24 11:55:33 +0530

    remove unnecessary code from main file

--- src/main/java/wallet/demo/DemoApplication.java
+++ src/main/java/wallet/demo/DemoApplication.java
@@ -2,13 +2,12 @@ package wallet.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
